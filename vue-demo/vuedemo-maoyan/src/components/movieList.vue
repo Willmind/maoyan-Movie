29fbc5a1aceabe580328fe92b28878b9494ce95e
Willmind <tanzhijingege@126.com>
2019-12-31 13:52:43 +0800

    更新显示列表

--- vue-demo/vuedemo-maoyan/src/components/movieList.vue
+++ vue-demo/vuedemo-maoyan/src/components/movieList.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <div class="movie-list">
+    <div @click="$router.push({name:'movieDetail',query:{id:mitem.id}})" class="movie-list">
       <div class="movie-img">
         <img :src="mitem.img.replace('w.h','128.180')" alt>
       </div>
       <div class="right-info">
         <ul class="movie-info">
-          <!--          name-->
           <li class="name">{{mitem.nm}}</li>
-          <li class="score" v-if="mitem.globalReleased && mitem.sc!==0">
-            观众评分
+          <li v-if="mitem.globalReleased && mitem.sc!==0" class="score">
+            观众评
             <span>{{mitem.sc}}</span>
           </li>
           <li style="font-size: 14px;color: #888" v-if="mitem.globalReleased && mitem.sc==0">暂无评分</li>
@@ -23,37 +22,21 @@
           <button v-if="mitem.globalReleased" class="buy-btn">购票</button>
           <button v-if="!mitem.globalReleased" class="pre-btn">预售</button>
         </div>
-
-
       </div>
-
-
     </div>
-
   </div>
-
 </template>
 
 <script>
     export default {
         name: "movieList",
+        props: ["mitem"], //通过mitem属性接收父组件的传值
         data() {
-            return {}
-        },
-        props: ["mitem"],
-        methods: {
-            dataDetail() {
-                this.$router.push({name: "movieDetail"})
-            },
-        },
-        mounted() {
-            console.log(this.mitem)
+            return {};
         }
-
-    }
+    };
 </script>
 
 <style scoped>
   @import "../assets/css/movie-list.css";
-
 </style>
